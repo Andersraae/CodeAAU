99eb69f4ebe4caa690ebb6795fed62cdee9ba8d2
Anders <andersraae01@gmail.com>
2023-02-27 12:22:09 +0100

    SLIAL og P2 uge 2

--- SW2/SLIAL/Week2/sdfsd.py
+++ SW2/SLIAL/Week2/sdfsd.py
@@ -1,4 +1,7 @@
-import numpy as np
-x = np.array([-1.1, 0.0, 3.6, -7.2])
-len(x)
-print(len(x))
+
+import matplotlib.pyplot as plt
+plt.plot([0, 1], [0, 1])
+plt.plot([1, 0], [0, 1], lw=3)
+plt.scatter([0], [.5])
+
+plt.show() 
