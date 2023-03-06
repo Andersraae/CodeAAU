a32823d2e3a8472146c6b82479982399e774c0a6
Anders <andersraae01@gmail.com>
2023-03-06 15:38:53 +0100

    Mere init og event listeners til local libary

--- SW2/SLIAL/Week2/sdfsd.py
+++ SW2/SLIAL/Week2/sdfsd.py
@@ -1,7 +1,14 @@
 
-import matplotlib.pyplot as plt
-plt.plot([0, 1], [0, 1])
-plt.plot([1, 0], [0, 1], lw=3)
-plt.scatter([0], [.5])
+# # # import matplotlib.pyplot as plt
+# # # plt.plot([0, 1], [0, 1])
+# # # plt.plot([1, 0], [0, 1], lw=3)
+# # # plt.scatter([0], [.5])
 
-plt.show() 
+# # # plt.show() 
+
+import math
+import numpy as np
+
+p = np.log(2, 8^2)
+
+print(p)
